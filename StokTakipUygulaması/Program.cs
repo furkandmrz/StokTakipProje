9ff5a9bd83fd424e9c0696a902fb03635c420f39
furkandmrz <furkanded00@gmail.com>
2025-12-29 23:34:21 +0300

    Full project upload
    Full project upload

---
+++ StokTakipUygulaması/Program.cs
@@ -0,0 +1,14 @@
+
+using StokTakipUygulamasý;
+using System;
+using System.Windows.Forms;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
